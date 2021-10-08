33f30996193dc731ac2114394c5ebd3a8dcee347
Mihirachyuta <61083462+Mihirachyuta@users.noreply.github.com>
2021-10-08 22:46:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from check import thrust_at_750
  
 USERNAME = "falconsofficial@gmail.com"
 PASSWORD = "lg7x6ie"
-propeller_range = list(range(18, 23))  # in inches
-pitch_range = list(range(8, 13))
+propeller_range = list(range(18, 24))  # in inches
+pitch_range = list(range(8, 15))
+blade_range = list(range(2,4))
  
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
@@ -87,12 +88,16 @@ motortype = driver.find_element_by_id("inMType")
  
  
 proptype = driver.find_element_by_id("inPType")
+proptype.send_keys("APC Electric E")
 propdiameter = driver.find_element_by_id("inPDiameter")
 proppitch = driver.find_element_by_id("inPPitch")
 flightspeed = driver.find_element_by_id("inPSpeed")
 flightspeed.send_keys(Keys.BACKSPACE)
 flightspeed.send_keys("47.8")
- 
+bladeCount=driver.find_element_by_id("inPBlades") 
+
+
+
 motorlist = []
 with open('prop1.csv') as file:
     reader = csv.reader(file)
@@ -101,7 +106,7 @@ with open('prop1.csv') as file:
  
  
 list_header = ['Propeller','Throttle','Current (DC)','Voltage (DC)','el. Power','Efficiency','Thrust','Spec. Thrust','Pitch Speed','Speed (level)','Motor Run Time']
- 
+sleep(2) 
 for motor in motorlist:
     data = []
     motormanufacturer.send_keys(motor[0])
@@ -115,40 +120,43 @@ for motor in motorlist:
                 propdiameter.send_keys(Keys.BACKSPACE)
             propdiameter.send_keys(str(diameter))
             for pitch in pitch_range:
-                data = []
-                proppitchcurrvalue = proppitch.get_attribute('value')
-                for i in range(len(proppitchcurrvalue)):
-                    proppitch.send_keys(Keys.BACKSPACE)
-                proppitch.send_keys(str(pitch))
-                calculatebtn = driver.find_element_by_name('btnCalculate')
-                calculatebtn.send_keys(Keys.RETURN)
-                sleep(0.5)
-                with open("tmp.html", "w") as f:
-                    f.write(driver.page_source.encode('utf-8').decode('ascii', 'ignore'))
-                with open("tmp.html") as f:
-                    soup = BeautifulSoup(f, 'html.parser')
-                    html = soup.find('table', {'id': 'rpmTable'}).find_all("tr")[2:]
-                    for element in html:
-                        sub_data = []
-                        for sub_element in element:
-                            try:
-                                sub_data.append(sub_element.get_text())
-                            except:
-                                continue
-                        del sub_data[7]
-                        del sub_data[8]
-                        del sub_data[9]
-                        del sub_data[10]
-                        data.append(sub_data)
-                batteryname='-'.join(battery.split('/'))      
-                dataFrame = pd.DataFrame(data = data, columns = list_header)
-                thrust = thrust_at_750(dataFrame)
-                thrust_to_weight=thrust/18000
-                if(thrust_to_weight<0.28):
-                    print(f'Rejected: results/{motor[1]}_{batteryname}_{diameter}_{pitch}.csv, thrust={thrust}, t:w={thrust_to_weight}')
-                else:
-                    print(f'Accepted: results/{motor[1]}_{batteryname}_{diameter}_{pitch}.csv, thrust={thrust}, t:w={thrust_to_weight}')
-                    dataFrame.to_csv(f'results/{motor[1]}_{batteryname}_{diameter}_{pitch}.csv')
+                for blade in blade_range:
+                    bladeCount.send_keys(Keys.BACKSPACE)
+                    bladeCount.send_keys(str(blade))
+                    data = []
+                    proppitchcurrvalue = proppitch.get_attribute('value')
+                    for i in range(len(proppitchcurrvalue)):
+                        proppitch.send_keys(Keys.BACKSPACE)
+                    proppitch.send_keys(str(pitch))
+                    calculatebtn = driver.find_element_by_name('btnCalculate')
+                    calculatebtn.send_keys(Keys.RETURN)
+                    sleep(0.25)
+                    with open("tmp.html", "w") as f:
+                        f.write(driver.page_source.encode('utf-8').decode('ascii', 'ignore'))
+                    with open("tmp.html") as f:
+                        soup = BeautifulSoup(f, 'html.parser')
+                        html = soup.find('table', {'id': 'rpmTable'}).find_all("tr")[2:]
+                        for element in html:
+                            sub_data = []
+                            for sub_element in element:
+                                try:
+                                    sub_data.append(sub_element.get_text())
+                                except:
+                                    continue
+                            del sub_data[7]
+                            del sub_data[8]
+                            del sub_data[9]
+                            del sub_data[10]
+                            data.append(sub_data)
+                    batteryname='-'.join(battery.split('/'))      
+                    dataFrame = pd.DataFrame(data = data, columns = list_header)
+                    thrust = thrust_at_750(dataFrame)
+                    thrust_to_weight=thrust/18000
+                    if(thrust_to_weight<0.28):
+                        print(f'Rejected: results/{motor[1]}_{batteryname}_{diameter}_{pitch}_{blade}.csv, thrust={thrust}, t:w={thrust_to_weight}')
+                    else:
+                        print(f'Accepted: results/{motor[1]}_{batteryname}_{diameter}_{pitch}_{blade}.csv, thrust={thrust}, t:w={thrust_to_weight}')
+                        dataFrame.to_csv(f'results/{motor[1]}_{batteryname}_{diameter}_{pitch}_{blade}.csv')
                 
                 
  
